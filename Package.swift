842d27af66c55522d5b03d7a0a32367259dfb885
Vyacheslav Khlichkin <vyacheslav.khlichkin@gmail.com>
2020-02-06 10:12:44 +0100

    - Sources path typo fixed

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0"))
   ],
   targets: [
-    .target(name: "RxUserDefaults_iOS", dependencies: ["RxSwift", "RxCocoa"], path: "Source"),
-    .target(name: "RxUserDefaults_tvOS", dependencies: ["RxSwift", "RxCocoa"], path: "Source"),
+    .target(name: "RxUserDefaults_iOS", dependencies: ["RxSwift", "RxCocoa"], path: "Sources"),
+    .target(name: "RxUserDefaults_tvOS", dependencies: ["RxSwift", "RxCocoa"], path: "Sources"),
     .testTarget(name: "RxUserDefaults-Tests_iOS", dependencies: ["RxUserDefaults_iOS"], path: "Tests"),
     .testTarget(name: "RxUserDefaults-Tests_tvOS", dependencies: ["RxUserDefaults_tvOS"], path: "Tests")
   ],
